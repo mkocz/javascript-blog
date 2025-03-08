f702fe3cad4be591a5c165120c2345a2758fc4b4
Maria Koczwara <mkoczwara@poczta.onet.pl>
2025-03-08 23:53:27 +0100

    Add class active to clicked link

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 const titleClickHandler = function(event){
   console.log('Link was clicked!', event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
+	
 	const activeLinks = document.querySelectorAll('.titles a.active');
 
 	for(let activeLink of activeLinks){
 		activeLink.classList.remove('active');
 	}
 
-  /* add class 'active' to the clicked link */
-		const activeArticles = document.querySelectorAll('article.active');
+  /* [DONE] add class 'active' to the clicked link */
 	
-		for(let activeArticle of activeArticles){
+	const clickedElement = this;
+	clickedElement.classList.add('active');
+
+  /* [DONE] remove class 'active' from all articles */
+	
+	const activeArticles = document.querySelectorAll('article.active');
+	
+	for(let activeArticle of activeArticles){
 		activeArticle.classList.remove('active');
 	}
-
-  /* remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
 
